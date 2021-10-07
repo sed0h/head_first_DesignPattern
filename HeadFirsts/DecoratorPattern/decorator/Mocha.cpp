ddfb4a7c3c89a22dd0d656830cde03022c926e59
zgt <1352861058@qq.com>
2021-10-07 23:07:11 +0800

    update getDescription

--- HeadFirsts/DecoratorPattern/decorator/Mocha.cpp
+++ HeadFirsts/DecoratorPattern/decorator/Mocha.cpp
@@ -2,7 +2,7 @@
 
 Mocha::Mocha(Beverage* beverage) { this->beverage = beverage; }
 
-std::string Mocha::getDescription() { return std::string();
+std::string Mocha::getDescription() {
   return beverage->getDescription() + ", Mocha";
 }
 
